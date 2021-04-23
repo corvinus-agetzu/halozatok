95b6778583b297ab8b1028ddb15373335de8c069
Soma Juhasz <soma1021@gmail.com>
2021-04-23 16:48:16 +0200

    9.ora

--- HajosTeszt/HajosTeszt/wwwroot/hetedik2.js
+++ HajosTeszt/HajosTeszt/wwwroot/hetedik2.js
@@ -1,47 +1,64 @@
-﻿window.onload = function () {
-    var csomag;
-    function kérdésMegjelenítés(kérdés) {
-        console.log(kérdés);
-        document.getElementById("kérdés_szöveg").innerText = kérdés.questionText
-        document.getElementById("válasz1").innerText = kérdés.answer1
-        document.getElementById("válasz2").innerText = kérdés.answer2
-        document.getElementById("válasz3").innerText = kérdés.answer3
-        if (kérdés.image == "") {
-            document.getElementById("kép").style.visibility = false
-        }
-        else {
-            document.getElementById("kép").style.visibility = true
-        }
+﻿
+        var kérdések
 
-        document.getElementById("kép1").src = "https://szoft1.comeback.hu/hajo/" + kérdés.image;
-        csomag = kérdés;
+       
+        function letöltésBefejeződött(d) {
+            console.log("Sikeres letöltés")
+            //console.log(d)
+            kérdések = d; console.log(kérdések);
+
+}
+
+window.onload = function () {
+    var i = 0;
+
+    válaszMegjelenítés(i);
+
+    kérdésMegjelenítés(i);
+
+    document.getElementById("előre").onclick = kattelőre;
+
+    document.getElementById("vissza").onclick = katthátra;
+
+    document.getElementById("válasz1").onclick = válasz1;
+    document.getElementById("válasz2").onclick = válasz2;
+    document.getElementById("válasz3").onclick = válasz3;
+
+
+
+
+    function kérdésMegjelenítés(i) {
+        let ide = document.getElementById("kérdés_szöveg");
+        ide.innerText = kérdések[i].questionText;
+
+        let idekep = document.getElementById("kép1");
+        idekep.src = "https://szoft1.comeback.hu/hajo/" + kérdések[i].image;
+
     }
 
-    function kérdésBetöltés(id) {
-        fetch(`/questions/${id}`)
-            .then(response => {
-                if (!response.ok) {
-                    console.error(`Hibás válasz: ${response.status}`)
-                }
-                else {
-                    return response.json()
-                }
-            })
-            .then(data => kérdésMegjelenítés(data));
-    }    
+    function válaszMegjelenítés(i) {
+        console.log(kérdések);
+        let válasz1 = document.getElementById("válasz1");
+        console.log(i);
+        
+        válasz1.innerText = kérdések[i].answer1;       
+        let válasz2 = document.getElementById("válasz2");
+        válasz2.innerText = kérdések[i].answer2;
+        let válasz3 = document.getElementById("válasz3");
+        válasz3.innerText = kérdések[i].answer3;
 
-
+    }
 
 
     function kattelőre() {
 
-        if (i < 800) {
+        if (i < kérdések.length - 1) {
             i = i + 1
         }
         else { i = 0 };
 
         alapszín();
-        kérdésBetöltés(i);
+        válaszMegjelenítés(i);
         kérdésMegjelenítés(i);
 
     };
@@ -50,16 +67,16 @@
         if (i > 0) {
             i = i - 1
         }
-        else i = 799;
+        else i = kérdések.length - 1;
 
         alapszín();
-        kérdésBetöltés(i);
+        válaszMegjelenítés(i);
         kérdésMegjelenítés(i);
     };
 
     function válasz1() {
 
-        if (1 == csomag.correctAnswer) {
+        if (1 == kérdések[i].correctAnswer) {
             document.getElementById("válasz1").classList.add("jó")
 
         }
@@ -69,7 +86,7 @@
 
     function válasz2() {
 
-        if (2 == csomag.correctAnswer) {
+        if (2 == kérdések[i].correctAnswer) {
             document.getElementById("válasz2").classList.add("jó")
         }
         else document.getElementById("válasz2").classList.add("rossz");
@@ -78,7 +95,7 @@
 
     function válasz3() {
 
-        if (3 == csomag.correctAnswer) {
+        if (3 == kérdések[i].correctAnswer) {
             document.getElementById("válasz3").classList.add("jó")
         }
         else document.getElementById("válasz3").classList.add("rossz");
@@ -96,17 +113,4 @@
         document.getElementById("válasz2").classList.add("kattinthato")
         document.getElementById("válasz3").classList.add("kattinthato")
     }
-
-  
-    i = 1;
-    kérdésBetöltés(i);
-
-
-    document.getElementById("előre").onclick = kattelőre;
-
-    document.getElementById("vissza").onclick = katthátra;
-
-    document.getElementById("válasz1").onclick = válasz1;
-    document.getElementById("válasz2").onclick = válasz2;
-    document.getElementById("válasz3").onclick = válasz3;
 }
